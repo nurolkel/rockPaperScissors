51592c3c45792d5ccab44d61bcb0cea0b980362f
Kelvin Nunez <knunezweb@gmail.com>
2021-12-13 15:38:37 -0500

    Added UI

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/* **
+* ! Start of Functions not in use
+**/
 function computerPlay() {
     const choices = ['rock','paper','scissors'];
     const randomNum = Math.floor(Math.random() * 3)
@@ -10,11 +13,12 @@ function computerPlay() {
 
 
 function player() {
-    const playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    
+    
     
     let result;
 
-    if (playerChoice === "") {
+    if (playerChoi === "") {
         result = "Don't Leave it Empty!!"
     } else if (playerChoice === "rock") {
         result = playerChoice;
@@ -31,6 +35,8 @@ function player() {
             );
     }
     return result
+
+
 }
 
 
@@ -107,25 +113,74 @@ function game() {
     return {playerCount, computerCount, tieCount, computerChoice, message};
 }
 
+/* **
+* ! End of Functions not in use
+**/
 
-const playerEl = document.createElement('div')
-const playerCounterEl = document.createElement('p')
-const computerEl = document.createElement('div')
-const computerCounterEl = document.createElement('p')
+const id = (id) => document.getElementById(id);
 
-function createTree() {
-    const runGame = game();
-    document.body.appendChild(playerEl)
-    document.body.appendChild(computerEl)
+const btn = id('btn')
+const playerEl = id('player')
+const computerEl = id('computer')
+const counterEl = id('gameCounter')
+const winnerEl = id('winner-board')
+const rock = id('rock')
+const paper = id('paper')
+const scissors = id('scissors')
 
-    playerCounterEl.textContent = runGame.playerCount
 
-    playerEl.appendChild(playerCounterEl)
+let counter = 0
+let playerScore = 0;
+let computerScore = 0;
+
+
+function startGame(e) {
     
-    computerCounterEl.textContent = runGame.playerCount
+    const computerSelection = computerPlay();
+    const playerSelection = e.target.getAttribute('id');
+    
+    let round = playRound(playerSelection,computerSelection)
 
-    computerEl.appendChild(computerCounterEl)
+   if (round === 'win') {
+       playerScore++
+       playerEl.textContent = `Your Score: ${playerScore}`
+       
+   } else if (round === 'lose') {
+       computerScore++
+       computerEl.textContent = `Computer Score: ${computerScore}`
+   }
+    counter++
+    counterEl.textContent = `Round ${counter}`
+  
+    if (counter >= 5) {
+        if (playerScore > computerScore) {
+            winnerEl.textContent = 'You are the Winner'
+            
+            return;
+        } else if (computerScore > playerScore) {
+            winnerEl.textContent = 'Computer Wins!'
+            counter = 0;
+            return;
+        } else if (computerScore === playerScore) {
+            counter++
+            winnerEl.textContent = "So many tie's, play again"
+
+        }
+    } 
 
 }
 
-createTree();
+function resetGame() {
+    counter = 0;
+    playerScore = 0;
+    computerScore = 0;
+
+    winnerEl.textContent = `Who Will the Winner Be?`
+    computerEl.textContent = `Computer Score: `
+    playerEl.textContent = `Your Score: `
+    counterEl.textContent = `Play a new game`
+}
+
+[rock,paper,scissors].forEach(item => item.addEventListener('click',startGame))
+btn.addEventListener('click', resetGame)
+
